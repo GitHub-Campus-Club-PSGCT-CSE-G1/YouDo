b5cee40c4360e038a50082628689fd5de62c3b4a
karthik srinivas <karthiksrinivas@karthiks-MacBook-Air.local>
2023-12-17 18:34:57 +0530

    Added in dark mode

--- src/YouDo/YouDo.java
+++ src/YouDo/YouDo.java
@@ -1,8 +1,12 @@
 package src.YouDo;
-
+import java.awt.Graphics2D;
+import java.awt.Graphics;
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -12,8 +16,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Properties;
-
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -23,9 +28,12 @@ import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 
-// import org.jdatepicker.JDatePicker;
 import org.jdatepicker.impl.JDatePanelImpl;
 import org.jdatepicker.impl.JDatePickerImpl;
 import org.jdatepicker.impl.UtilDateModel;
@@ -37,6 +45,14 @@ public class YouDo {
     private static JTable table;
 
     public static void main(String[] args) {
+        try {
+    UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+    UIManager.getLookAndFeelDefaults().put("TextField.caretForeground", Color.WHITE);
+} catch (Exception e) {
+    e.printStackTrace();
+}
+
+        
         // Initialize a user for testing
         currentUser = new User("Dhakkshin", "me@my.com");
 
@@ -50,7 +66,7 @@ public class YouDo {
         // Panel 2 - Task Input
         JPanel taskInputPanel = createTaskInputPanel();
 
-        // Panel 3 - Display Area (JTable) and Submit Button
+        // Panel 3 - Display Area (JTable)
         JPanel displayPanel = createDisplayPanel();
 
         // Add panels to the main frame
@@ -86,41 +102,61 @@ public class YouDo {
         JLabel instructionLabel = new JLabel("Here, you have to do!!");
         welcomePanel.add(welcomeLabel);
         welcomePanel.add(instructionLabel);
+        // welcomepanel style
+        welcomeLabel.setBackground(Color.BLACK);
+        welcomeLabel.setForeground(Color.WHITE);
+        welcomePanel.setBackground(Color.BLACK);
+        welcomePanel.setForeground(Color.WHITE);
+        instructionLabel.setForeground(Color.WHITE);
         return welcomePanel;
     }
 
     private static JPanel createTaskInputPanel() {
-        JPanel taskInputPanel = new JPanel(new GridLayout(7, 2)); // Increased rows for the new component
-        JLabel taskLabel = new JLabel("Enter your task here:");
+        JPanel taskInputPanel = new JPanel(new GridLayout(8, 2, 0, 10)); // Increased rows for the new component and added vertical gap
+        JLabel taskLabel = new JLabel("  Enter your task here:");
         JTextField taskField = new JTextField(20);
         JLabel priorityLabel = new JLabel("Enter Priority:");
         JTextField priorityField = new JTextField(20);
         JLabel descriptionLabel = new JLabel("Enter Description:");
         JTextField descriptionField = new JTextField(20);
-    
+
+        // Set background and foreground for taskLabel
+        taskLabel.setBackground(Color.BLACK);
+        taskLabel.setForeground(Color.WHITE);
+
+        // Set background and foreground for priorityLabel
+        priorityLabel.setBackground(Color.BLACK);
+        priorityLabel.setForeground(Color.WHITE);
+
+        // Set background and foreground for descriptionLabel
+        descriptionLabel.setBackground(Color.BLACK);
+        descriptionLabel.setForeground(Color.WHITE);
+
         // Add JDatePicker for Due Date
         JLabel dueDateLabel = new JLabel("Select Due Date:");
         UtilDateModel model = new UtilDateModel();
         Properties properties = new Properties();
         JDatePanelImpl datePanel = new JDatePanelImpl(model, properties);
         JDatePickerImpl datePicker = new JDatePickerImpl(datePanel, new DateLabelFormatter());
-    
+
+        // Set background and foreground for dueDateLabel
+        dueDateLabel.setBackground(Color.BLACK);
+        dueDateLabel.setForeground(Color.WHITE);
+
+        JButton resetButton = new JButton("Reset");
         JButton submitButton = new JButton("Submit");
-    
-        submitButton.addActionListener(new ActionListener() {
+        styleResetButton(resetButton);
+        styleSubmitButton(submitButton);
+
+        // Style for text fields and date picker
+        styleTextField(taskField);
+        styleTextField(priorityField);
+        styleTextField(descriptionField);
+        styleDatePicker(datePicker);
+
+        resetButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-                String taskName = taskField.getText();
-                String taskPriority = priorityField.getText();
-                String taskDescription = descriptionField.getText();
-    
-                // Retrieve the selected due date from JDatePicker
-                Date dueDate = (Date) datePicker.getModel().getValue();
-    
-                // Continue with task creation
-                Task newTask = new Task(taskName, taskDescription, taskPriority, dueDate);
-                currentUser.addTask(newTask);
-                updateDisplayArea(currentUser);
-    
                 // Reset input fields
                 taskField.setText("");
                 priorityField.setText("");
@@ -128,7 +164,34 @@ public class YouDo {
                 datePicker.getModel().setValue(null); // Reset the JDatePicker
             }
         });
-    
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String taskName = taskField.getText();
+                String taskPriority = priorityField.getText();
+                String taskDescription = descriptionField.getText();
+
+                // Retrieve the selected due date from JDatePicker
+                Date dueDate = (Date) datePicker.getModel().getValue();
+
+                // Continue with task creation
+                Task newTask = new Task(taskName, taskDescription, taskPriority, dueDate);
+                currentUser.addTask(newTask);
+                updateDisplayArea(currentUser);
+
+                // Reset input fields
+                taskField.setText("");
+                priorityField.setText("");
+                descriptionField.setText("");
+                datePicker.getModel().setValue(null); // Reset the JDatePicker
+            }
+        });
+
+        // Adding an empty label to create space between buttons and text fields
+        taskInputPanel.add(new JLabel());
+        taskInputPanel.add(new JLabel());
+
         taskInputPanel.add(taskLabel);
         taskInputPanel.add(taskField);
         taskInputPanel.add(priorityLabel);
@@ -136,54 +199,130 @@ public class YouDo {
         taskInputPanel.add(descriptionLabel);
         taskInputPanel.add(descriptionField);
         taskInputPanel.add(dueDateLabel);
-        taskInputPanel.add(datePicker);  // Directly add the JDatePicker without wrapping in a panel
-        taskInputPanel.add(new JPanel()); // Empty panel for spacing
+        taskInputPanel.add(datePicker);
+
+        taskInputPanel.add(resetButton);
         taskInputPanel.add(submitButton);
-    
+
+        // Set background for taskInputPanel
+        taskInputPanel.setBackground(Color.BLACK);
+        taskInputPanel.setForeground(Color.WHITE);
+
         return taskInputPanel;
     }
-    
 
+    private static void styleResetButton(JButton button) {
+        button.setBackground(Color.GRAY); // Set background color to light gray
+        button.setForeground(Color.BLACK);
+        button.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20)); // Adjust the values as needed
+        button.setOpaque(true);
+        button.setBorderPainted(false); // Remove default button border
+        button.setFocusPainted(false); // Remove focus border
+        button.setContentAreaFilled(true); // Ensure that the background is filled
+        button.setFont(button.getFont().deriveFont(16f)); // Optional: Set font size
+        button.setBorder(new RoundedBorder(30));
+    }
+
+    private static void styleSubmitButton(JButton button) {
+        button.setBackground(new Color(0, 128, 0)); // Set background color to green
+        button.setForeground(Color.WHITE);
+        button.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20)); // Adjust the values as needed
+        button.setOpaque(true);
+        button.setBorderPainted(false); // Remove default button border
+        button.setFocusPainted(false); // Remove focus border
+        button.setContentAreaFilled(true); // Ensure that the background is filled
+        button.setFont(button.getFont().deriveFont(16f));
+                button.setBorder(new RoundedBorder(30));
+ // Optional: Set font size
+    }
+private static void styleTextField(JTextField textField) {
+    textField.setBackground(new Color(30, 30, 30));
+    textField.setForeground(Color.WHITE);
+    textField.setCaretColor(Color.BLACK); // Set caret color to be visible
+    textField.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2)); // Use a LineBorder
+    textField.setMargin(new Insets(5, 5, 5, 5));
+}
+private static void styleDatePicker(JDatePickerImpl datePicker) {
+    datePicker.setBackground(Color.GRAY); // Set background color to gray for the entire date picker panel
+    datePicker.getJFormattedTextField().setBackground(Color.GRAY); // Set background color to gray for the text field
+    datePicker.setForeground(Color.WHITE);
+    datePicker.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2)); // Optional: Add a border
+    datePicker.getJFormattedTextField().setCaretColor(Color.WHITE);
+    datePicker.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+}
     private static JPanel createDisplayPanel() {
         JPanel displayPanel = new JPanel(new BorderLayout());
-
+    
         // Table headers and initial data
         String[] columnNames = {"S.No", "ID", "Name", "Description", "Priority", "Due Date", "Status", "Creation Date"};
         Object[][] data = {};
-
+    
         // Create a DefaultTableModel with data and column names
         tableModel = new DefaultTableModel(data, columnNames);
-
+    
         table = new JTable(tableModel);
+        CenterRenderer centerRenderer = new CenterRenderer();
+        for (int i = 0; i < table.getColumnCount(); i++) {
+            table.getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
+        }
         JScrollPane scrollPane = new JScrollPane(table);
-
+        scrollPane.getViewport().setBackground(Color.BLACK);
         // Add a right-click listener to the table
         table.addMouseListener(new TableRightClickListener());
-
+    
         JButton refreshButton = new JButton("Refresh");
         refreshButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
                 updateDisplayArea(currentUser);
             }
         });
-
+    
         displayPanel.add(scrollPane, BorderLayout.CENTER);
         displayPanel.add(refreshButton, BorderLayout.SOUTH);
-
+    
+        // Set background for displayPanel (including white space)
+        displayPanel.setBackground(Color.BLACK);
+        displayPanel.setForeground(Color.WHITE);
+        JTableHeader header = table.getTableHeader();
+    header.setDefaultRenderer(new CenterHeaderRenderer());
+    
+        // Set background for table
+        table.setBorder(null);
+        table.setShowHorizontalLines(false);
+        table.setShowVerticalLines(false);
+        table.setBackground(Color.BLACK);
+        table.setForeground(Color.WHITE);
+    
+        // Set background for table headers
+        table.getTableHeader().setBackground(Color.BLACK);
+        table.getTableHeader().setForeground(Color.WHITE);
+    
+        // Set background for refreshButton
+        refreshButton.setBackground(new Color(0, 128, 0)); // Green color
+        refreshButton.setForeground(Color.WHITE);
+        refreshButton.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20)); // Adjust the values as needed
+        refreshButton.setOpaque(true);
+        refreshButton.setBorderPainted(false); // Remove default button border
+        refreshButton.setFocusPainted(false); // Remove focus border
+        refreshButton.setContentAreaFilled(true); // Ensure that the background is filled
+        refreshButton.setFont(refreshButton.getFont().deriveFont(16f)); // Optional: Set font size
+        refreshButton.setBorder(new RoundedBorder(30));
+    
         return displayPanel;
     }
-
-    private static void updateDisplayArea(User user) {
+            private static void updateDisplayArea(User user) {
         tasks = user.getTasks();
 
         // Clear the existing data in the table
         tableModel.setRowCount(0);
 
+
         for (int i = 0; i < tasks.size(); i++) {
             Task task = tasks.get(i);
 
-            Object[] rowData = { i + 1, task.getId(), task.getName(), task.getDescription(), task.getPriority(),
-                    task.getDueDate(), task.getStatus(), task.getCreationDate() };
+            Object[] rowData = {i + 1, task.getId(), task.getName(), task.getDescription(), task.getPriority(),
+                    task.getDueDate(), task.getStatus(), task.getCreationDate()};
 
             tableModel.addRow(rowData);
         }
@@ -288,4 +427,53 @@ public class YouDo {
         }
     }
 
+    // Helper class for creating rounded borders
+// Helper class for creating rounded borders
+// Helper class for creating rounded borders
+// Helper class for creating rounded borders
+static class RoundedBorder implements Border {
+    private int radius;
+
+    public RoundedBorder(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.radius, this.radius, this.radius, this.radius);
+    }
+
+    @Override
+    public boolean isBorderOpaque() {
+        return true;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        if (c instanceof JComponent) {
+            Graphics2D g2d = (Graphics2D) g.create();
+
+            int arcDiameter = radius * 2;
+            g2d.drawRoundRect(x, y, width - 1, height - 1, arcDiameter, arcDiameter);
+
+            g2d.dispose();
+        }
+    }
 }
+private static class CenterRenderer extends DefaultTableCellRenderer {
+    public CenterRenderer() {
+        setHorizontalAlignment(JLabel.CENTER);
+    }
+}
+private static class CenterHeaderRenderer extends DefaultTableCellRenderer {
+    public CenterHeaderRenderer() {
+        setHorizontalAlignment(JLabel.CENTER);
+        setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5)); // Adjust the padding as needed
+    }
+}
+
+
+}
+
+
+
